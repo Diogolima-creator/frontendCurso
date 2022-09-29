36932d08bb2b4b42312d487023b3a701fb31493b
Diogolima-creator <dbznetudo@gmail.com>
2022-09-29 15:23:09 -0400

    create two functions and refactor

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import * as C  from './styles';
+import * as C from './styles';
 import { Eye, EyeSlash } from 'phosphor-react'
 import { useEffect, useState } from 'react';
 import CountUp from 'react-countup';

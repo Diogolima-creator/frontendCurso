64471290fe07c3c0d70e05c4ead6ad460602b2ca
Diogolima-creator <dbznetudo@gmail.com>
2022-11-17 00:39:38 -0400

    oi

--- src/http/news/index.tsx
+++ src/http/news/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 const API_KEY = "834bc2e5cfe44c40b141cf46157eb0d0"; // api news
 
-export async function newsRequest(){
+/*export async function newsRequest(){
   const result = await axios.get('https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=834bc2e5cfe44c40b141cf46157eb0d0',
   {
       headers:{
@@ -14,4 +14,18 @@ export async function newsRequest(){
   }else{
       alert('deu ruim')
   }
-}
+}*/
+
+export async function newsRequest(){
+    let result = await fetch('https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=834bc2e5cfe44c40b141cf46157eb0d0',{
+                  method: 'GET',
+                  headers:{
+                      'Content-type' : 'application/json',
+                  }
+              }).then((res) => res.json())
+    if(result.data.status === 'ok'){
+        return result.data
+    }else{
+        alert('deu ruim')
+    }
+  }
